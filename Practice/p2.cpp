44530a6961fba9fefcf715da40ee79a287365da7
codewithdivu <mavadiyadivyesh56@gmail.com>
2023-03-11 21:25:10 +0530

    again changes

--- Practice/p2.cpp
+++ Practice/p2.cpp
@@ -1,28 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void findMax(int arr[], int &maxu, int size)
-{
-    if (size == 0)
-    {
-        return;
-    }
-
-    if (arr[0] > maxu)
-    {
-        maxu = arr[0];
-    }
-
-    findMax(arr + 1, maxu, size - 1);
-}
-
 int main(int argc, char const *argv[])
 {
-    int arr[] = {1, 88, 45, 97, 45, 2, 444, 67, 77};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int maxu = INT_MIN;
-
-    findMax(arr, maxu, size);
-    cout << "maximum is " << maxu << endl;
+    
     return 0;
 }
