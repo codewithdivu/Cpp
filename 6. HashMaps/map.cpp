5b9787af2dfd5f15e7a5b9b234871b759af014ef
codewithdivu <mavadiyadivyesh56@gmail.com>
2023-03-21 09:53:11 +0530

    few changes

--- 6. HashMaps/map.cpp
+++ 6. HashMaps/map.cpp
@@ -7,3 +7,14 @@ int main(int argc, char const *argv[])
     map<int, int> mp;
     return 0;
 }
+
+// map
+/*
+---------------------------------------------
+|   Operation	        | Time Complexity   |
+|   Insert	            |   O(log(n))       |
+|   Access by Key	    |   O(log(n))       |
+|   Remove by Key	    |   O(log(n))       |
+|   Find/Remove Value   |	O(log(n))       |
+---------------------------------------------
+*/
--- 6. HashMaps/unordered_map.cpp
+++ 6. HashMaps/unordered_map.cpp
@@ -7,3 +7,14 @@ int main(int argc, char const *argv[])
     unordered_map<int, int> um;
     return 0;
 }
+
+// unordered_map
+/*
+---------------------------------------------
+|   Operation	        | Time Complexity   |
+|   Insert	            |   O(1)            |
+|   Access by Key	    |   O(1)            |
+|   Remove by Key	    |   O(1)            |
+|   Find/Remove Value   |	--              |
+---------------------------------------------
+*/
