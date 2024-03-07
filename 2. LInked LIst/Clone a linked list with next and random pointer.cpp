6aa1e6f1fb3bd5786345e89aee6a79c817244036
MAVADIYA DIVYESHKUMAR <74129812+codewithdivu@users.noreply.github.com>
2024-03-07 13:59:32 +0530

    Update Clone a linked list with next and random pointer.cpp

--- 2. LInked LIst/Clone a linked list with next and random pointer.cpp
+++ 2. LInked LIst/Clone a linked list with next and random pointer.cpp
@@ -43,3 +43,81 @@ class Solution
     }
 
 };
+
+
+
+// approach-3
+
+class Solution
+{
+    public:
+    Node *copyList(Node *head)
+    {
+        
+        // 1. create clone list
+        
+        Node* dummy = new Node(-1);
+        Node* temp = dummy;
+        Node* femp = head;
+        
+        while(femp!=NULL){
+            Node* newNode = new Node(femp->data);
+            temp->next = newNode;
+            temp = temp->next;
+            femp = femp->next;
+        }
+        
+        // 2. add cloneNodes between original nodes
+        
+        Node* originalHead = head;
+        Node* cloneHead = dummy->next;
+        
+        while(originalHead!=NULL && cloneHead!=NULL){
+            Node* next = originalHead->next;
+            originalHead->next = cloneHead;
+            originalHead = next;
+            
+            next = cloneHead->next;
+            cloneHead->next = originalHead;
+            cloneHead = next;
+        }
+        
+        // 3. set random pointers
+        
+        Node* gemp = head;
+        
+        while(gemp!=NULL){
+            if(gemp->next!=NULL){
+                if(gemp->arb != NULL){
+                    gemp->next->arb = gemp->arb->next;
+                }else{
+                    gemp->next->arb = gemp->arb;
+                }
+            }
+            gemp = gemp->next->next;
+        }
+        
+        
+        // 4. revert the changes
+                
+        Node* originalNode = head;
+        Node* cloneNode = dummy->next;
+        
+        
+        while(originalNode!=NULL && cloneNode!=NULL){
+            originalNode->next = cloneNode->next;
+            originalNode = originalNode->next;
+            
+            if(originalNode!=NULL){
+                cloneNode->next = originalNode->next;
+            }
+            cloneNode = cloneNode->next;
+        }
+        
+        
+        // 5. return answer
+        
+        return dummy->next;
+    }
+
+};
